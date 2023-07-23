f091b062dabf0ca434ff22683e468830606c0cca
NASA-INTERNET <136007372+NASA-INTERNET@users.noreply.github.com>
2023-07-23 20:24:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def getenv(var): return environ.get(var) or DATA.get(var, None)
 bot_token = getenv("TOKEN")
 api_hash = getenv("HASH") 
 api_id = getenv("ID")
-AUTHORIZED_CHATS = getenv("CHATS")
+authorized_chats = getenv("CHATS")
 app = Client("my_bot",api_id=api_id, api_hash=api_hash,bot_token=bot_token,authorized_chats=chats)  
 
 
